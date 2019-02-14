0d50d7b504b320d37b5770482b7940cc703650c9
Mason Proffitt <mason.louis.proffitt@cern.ch>
2019-02-14 03:50:26 +0100

    add some fancier stuff for rdataframes

--- rdataframes.C
+++ rdataframes.C
@@ -1,8 +1,28 @@
-ROOT::RDataFrame df("recoTree", "/afs/cern.ch/work/m/mproffit/public/calratio/signal_HSS_LLP_mH1000_mS50_lt9m_ntuples2016dataAnalysis_paperVersion.root");
-auto h_HTMiss = df.Histo1D("event_HTMiss");
-TCanvas c;
-h_HTMiss->Draw();
-h_HTMiss->SetTitle(";HTMiss;Number of events");
-auto *x_axis = h_HTMiss->GetXaxis();
-x_axis->SetRangeUser(0, 1000);
-c.Print("HTMiss.png");
+void rdataframes() {
+	ROOT::RDataFrame df("recoTree", "/afs/cern.ch/work/m/mproffit/public/calratio/signal_HSS_LLP_mH1000_mS50_lt9m_ntuples2016dataAnalysis_paperVersion.root");
+
+	std::cout << "Number of events before cut: " << df.Count().GetValue() << std::endl;
+	auto h_HTMiss_before_cut = df.Histo1D({"", ";HTMiss before cut;Number of events", 100, 0.0, 1000.0}, "event_HTMiss");
+
+	auto cut = df.Filter(
+		[](const std::vector<double> CalibJet_pT) {
+			const auto n_high_pt_jets = std::count_if(
+				CalibJet_pT.cbegin(),
+				CalibJet_pT.cend(),
+				[](const double pt){return pt > 100.0;});
+			return n_high_pt_jets >= 2;
+		},
+		{"CalibJet_pT"}
+	);
+	std::cout << "Number of events after cut: " << cut.Count().GetValue() << std::endl;
+	auto h_HTMiss_after_cut = cut.Histo1D({"", ";HTMiss after cut;Number of events", 100, 0.0, 1000.0}, "event_HTMiss");
+
+	TCanvas c;
+	h_HTMiss_before_cut->Draw();
+	c.Print("HTMiss_before_cut.png");
+	c.Clear();
+
+	h_HTMiss_after_cut->Draw();
+	c.Print("HTMiss_after_cut.png");
+	c.Clear();
+}
